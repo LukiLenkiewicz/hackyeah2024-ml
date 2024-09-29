7b9996b014627926549b287488935d9441f8a43b
julia <juliafarganus@gmail.com>
2024-09-29 02:17:54 +0200

    basic ui

--- scripts/app.py
+++ scripts/app.py
@@ -1,32 +1,86 @@
 import streamlit as st
-# import ollama
+import ollama
 
 # from scripts folder run the command below
 # streamlit run app.py
 
+avatars = {
+    "user": "../data/avatars/human.png",
+    "assistant": "../data/avatars/logo.png",
+}
+
+css_classes = {
+    "user": "user-container",
+    "assistant": "assistant-container",
+}
+
+# Define custom CSS for chat message box
+custom_css = """
+<style>
+    
+    /* Custom style for the user chat message */
+    .stChatMessage.eeusbqq4 {
+        background-color: #3E7E8C; /* Light blue background */
+        
+    }
+
+    /* Custom style for the avatar */
+    .stChatMessage.st-emotion-cache-4oy321.eeusbqq4 {
+        background-color: #A67153;
+    }
+    
+    [data-baseweb=textarea] {
+        color: black;
+        border: 1px solid #024059;
+    }
+    
+    [type=textarea] {
+        color: black;
+        caret-color: black;
+    }
+    
+    #easytalk {
+        color: black;
+    }
+    
+    .stSpinner {
+        color: black;
+    }
+    
+    header {
+        color: black;
+    }
+    
+</style>
+"""
+
 st.title("EasyTalk")
+st.markdown(custom_css, unsafe_allow_html=True)
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    st.session_state.messages.append({"role": "assistant", "content": "Hi, how can I help you?"})
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    with st.chat_message(message["role"], avatar=avatars[message["role"]]):
+        st.markdown(message['content'])
 
 prompt = st.chat_input("I have a problem with ...")
+
 if prompt:
 
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar=avatars["user"]) as uchat:
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # result = ollama.chat(model="llama3", messages=st.session_state.messages)
-    # response = result["message"]["content"]
+    with st.spinner("Processing ..."):
+        result = ollama.chat(model="llama3", messages=st.session_state.messages)
+        response = result["message"]["content"]
+        with st.chat_message("assistant", avatar=avatars["assistant"]):
+            st.markdown(response)
+    # response = f"Echoing: {prompt}"
     # with st.chat_message("assistant"):
     #     st.markdown(response)
-    response = f"Echoing: {prompt}"
-    with st.chat_message("assistant"):
-        st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
